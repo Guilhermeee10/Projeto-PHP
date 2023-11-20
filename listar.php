6abc09b298e3ccfa4016509c45f6f421cd993eaa
Augusto051103 <150140841+Augusto051103@users.noreply.github.com>
2023-11-20 20:17:38 -0300

    Add files via upload

---
+++ listar.php
@@ -0,0 +1,24 @@
+<?php 
+include_once("config.inc.php");
+
+$result_usuarios = "SELECT * FROM formulario";
+$resultado_usuarios = mysqli_query($conexao, $result_usuarios);
+
+if ($resultado_usuarios) {
+    echo "<h1>Lista de Usuários</h1>";
+
+    while ($row_usuario = mysqli_fetch_assoc($resultado_usuarios)) {
+        echo "id: " . $row_usuario['id'] . "<br>";
+        echo "nome: " . $row_usuario['nome'] . "<br>";
+        echo "sobrenome: " . $row_usuario['sobrenome'] . "<br>";
+        echo "email: " . $row_usuario['email'] . "<br>";
+        echo "sexo: " . $row_usuario['sexo'] . "<br>";
+        echo "assunto: " . $row_usuario['assunto'] . "<br>";
+        echo "mensagem: " . $row_usuario['mensagem'] . "<br><hr>";
+    }
+} else {
+    echo "Erro na execução da consulta: " . mysqli_error($conexao);
+}
+
+mysqli_close($conexao);
+?>
